e30e152423771a84d0f552b4a4a03a8eff936599
antdangcs <antdang.cs@gmail.com>
2025-03-26 11:18:02 +0700

    Add Doom Emacs config

--- .config/nvim/lua/a/plugins/lsp-zero.lua
+++ .config/nvim/lua/a/plugins/lsp-zero.lua
@@ -110,7 +110,7 @@ return {
 			end)
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "ts_ls" }, -- Ensure tsserver is installed
+				ensure_installed = {}, -- Ensure tsserver is installed
 				handlers = {
 					-- this first function is the "default handler"
 					-- it applies to every language server without a "custom handler"
